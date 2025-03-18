f176578846874c5d37283440fb561c836578999b
gspetro-NOAA <gillian.petro@noaa.gov>
2025-03-18 18:18:39 +0000

    add srw-v3.0.0 docs

---
+++ ver-srw-3.0.0/emcsfc_snow2mdl/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme',['README',['../md__scratch2_NAGAPE_epic_Gillian_8Petro_UFS_UTILS-epic_sorc_emcsfc_snow2mdl_8fd_docs_README.html',1,'']]]
+];
---
+++ ver-srw-3.0.0/sfc_climo_gen/search/all_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readme',['README',['../md__scratch2_NAGAPE_epic_Gillian_8Petro_UFS_UTILS-epic_sorc_sfc_climo_gen_8fd_docs_README.html',1,'']]],
+  ['read_5fsetup_5fnamelist',['read_setup_namelist',['../namespaceprogram__setup.html#ac52f11da9f12e0cf9ff87fae136f0cde',1,'program_setup']]]
+];
